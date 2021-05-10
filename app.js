1fae3890fdb92ef618e04d33f3c5a148b54c6820
ariellongoria <ariello97@hotmail.com>
2021-05-09 22:31:21 -0500

    modularize webpage template

---
+++ app.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const generatePage = require("./src/page-template.js");
+
+const profileDataArgs = process.argv.slice(2);
+const [firstName, github] = profileDataArgs;
+
+fs.writeFile("./index.html", generatePage(firstName, github), (err) => {
+    if (err) throw err;
+
+    console.log("Portfolio complete! Check out index.html to see the output!");
+});
